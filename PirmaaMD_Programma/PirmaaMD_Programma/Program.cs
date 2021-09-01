4b366b5115539d5ef07989400312d746292fcad8
Edgars Raginskis <raginskis@gmail.com>
2021-09-02 00:21:01 +0300

    Created a program.

---
+++ PirmaaMD_Programma/PirmaaMD_Programma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PirmaaMD_Programma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
